6c2cbe447156e45cdc2fb60de5c780e4fe2e0038
Wojciech Grabowski <wojtek@wojciech-grabowski.pl>
2020-03-20 09:51:47 +0100

    Update tests

--- src/admiralize.spec.ts
+++ src/admiralize.spec.ts
@@ -2,8 +2,18 @@ import { admiralize } from './admiralize';
 import { DEFAULT_NAME } from './constants';
 
 describe('admiralize()', () => {
-	test('When input is undefined or empty it should return default value', () => {
-		expect(admiralize(null)).toEqual(DEFAULT_NAME);
-		expect(admiralize('')).toEqual(DEFAULT_NAME);
-	});
-})
+  test('When input is undefined or empty it should return default value', () => {
+    expect(admiralize(null)).toEqual(DEFAULT_NAME);
+    expect(admiralize('')).toEqual(DEFAULT_NAME);
+  });
+
+  test('When input ends with o,a,i,e,u should remove it in returned value and add "ow" suffix', () => {
+    expect(admiralize('Duda')).toEqual('dudow');
+    expect(admiralize('Kowalska')).toEqual('kowalskow');
+  });
+
+  test('When input ends other letter it should remove return input with "ow" suffix', () => {
+    expect(admiralize('Wał')).toEqual('wałow');
+    expect(admiralize('Suchar')).toEqual('sucharow');
+  });
+});
