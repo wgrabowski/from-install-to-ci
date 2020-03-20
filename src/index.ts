09cf1df50499117f25e4f75a84b2677df998a853
Wojciech Grabowski <wojtek@wojciech-grabowski.pl>
2020-03-20 09:42:39 +0100

    Update lintstagedrc

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,13 @@
 import { admiralize } from './admiralize';
 
 (() => {
-	const input = document.querySelector('#name');
-	const button = document.querySelector('#button');
-	const output = document.querySelector('#output');
+  const input = document.querySelector('#name');
+  const button = document.querySelector('#button');
+  const output = document.querySelector('#output');
 
+  const generate = () => output.innerHTML = admiralize((input as any).value);
 
+  const init = () => button.addEventListener('click', generate);
 
-	const generate = () => output.innerHTML = admiralize((input as any).value);
-
-	const init = () => button.addEventListener('click', generate);
-
-	init();
+  init();
 })();
